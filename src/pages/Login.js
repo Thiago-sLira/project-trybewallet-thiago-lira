a94e084a2366dd4f30b20d67ad1d29b8117f3b19
Thiago Lira <thiago.slira@outlook.com>
2022-12-08 15:56:39 -0300

    Ajustado as rotas da aplicação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <fieldset>
+          <h1>Trybe</h1>
+        </fieldset>
+      </div>
+    );
   }
 }
 
