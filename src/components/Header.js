16f20bda0faf30fe27f4ae676e079e78ed398104
Thiago Lira <thiago.slira@outlook.com>
2022-12-08 17:31:24 -0300

    Criado header. Requisito 2 concluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,15 @@ import { string } from 'prop-types';
 
 class Header extends Component {
   render() {
+    const exchange = 'BRL';
     const { userEmail } = this.props;
     return (
       <header>
-        <h5 data-testid="email-field">{ `Email: ${userEmail}` }</h5>
+        <section>
+          <h5 data-testid="email-field">{ `Email: ${userEmail}` }</h5>
+          <h5 data-testid="total-field">{ `Despesa Total: ${0}` }</h5>
+          <h5 data-testid="header-currency-field">{ `${exchange}` }</h5>
+        </section>
       </header>
     );
   }
