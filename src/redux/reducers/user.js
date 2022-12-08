7a7fb2cbe3e9e54dbfca14ac7d25e9d9dbd48257
Thiago Lira <thiago.slira@outlook.com>
2022-12-08 17:02:34 -0300

    Requisito 1 concluído

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -2,24 +2,14 @@
 import { RECEIVE_USER_EMAIL } from '../actions/index';
 
 const INITIAL_STATE = {
-  user: {
-    email: '', // string que armazena o email da pessoa usuária
-  },
-  wallet: {
-    currencies: [], // array de string
-    expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
-    editor: false, // valor booleano que indica de uma despesa está sendo editada
-    idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
-  },
+  email: '', // string que armazena o email da pessoa usuária
 };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case RECEIVE_USER_EMAIL: return {
     ...state,
-    user: {
-      email: action.payload,
-    },
+    email: action.payload,
   };
   default: return state;
   }
