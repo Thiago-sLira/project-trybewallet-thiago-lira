2d58f0a7ef1d675be5ba7a7cd6781e29d77e37a6
Thiago Lira <thiago.slira@outlook.com>
2022-12-10 16:26:22 -0300

    Component Table: Criado connect e propTypes da chave wallet.expenses

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { arrayOf, string, shape, number } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>Table</div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ expenses }) => ({
+  expenses,
+});
+
+Table.defaultProps = {
+  expenses: [{
+    id: 0,
+    value: '',
+    description: '',
+    currency: '',
+    method: '',
+    tag: '',
+    exchangesRates: {},
+  }],
+};
+
+Table.propTypes = {
+  expenses: arrayOf(shape({
+    id: number,
+    value: string,
+    description: string,
+    currency: string,
+    method: string,
+    tag: string,
+    exchangesRates: shape({}),
+  })),
+};
+
+export default connect(mapStateToProps)(Table);
